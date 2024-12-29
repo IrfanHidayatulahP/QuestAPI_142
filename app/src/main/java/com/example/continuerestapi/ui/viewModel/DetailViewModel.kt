b4a5890367bf455becbc02fd7890041f3efb4014
IrfanHidayatulahP <irfanhputra400@gmail.com>
2024-12-29 22:27:28 +0700

    membuat fungsi Mahasiswa.toDetailUiEvent membuat data class DetailUiState

---
+++ app/src/main/java/com/example/continuerestapi/ui/viewModel/DetailViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.continuerestapi.ui.viewModel
+
+import com.example.continuerestapi.model.Mahasiswa
+
+class DetailViewModel {
+}
+
+data class DetailUiState(
+    val detailUiEvent: InsertUiEvent = InsertUiEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == InsertUiEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != InsertUiEvent()
+}
+
+fun Mahasiswa.toDetailUiEvent() : InsertUiEvent {
+    return InsertUiEvent(
+        nim = nim,
+        nama = nama,
+        alamat = alamat,
+        jenisKelamin = jenisKelamin,
+        kelas = kelas,
+        angkatan = angkatan
+    )
+}
