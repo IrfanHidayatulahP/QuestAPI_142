7ffddf0289409276477f471608fd071c73b29def
IrfanHidayatulahP <irfanhputra400@gmail.com>
2024-12-29 21:58:41 +0700

    memperbaiki struktur file dan memperbaiki isi MahasiswaApps

--- app/src/main/java/com/example/continuerestapi/ui/viewModel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/continuerestapi/ui/viewModel/PenyediaViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import com.example.continuerestapi.widget.MahasiswaApps
+import com.example.continuerestapi.ui.viewModel.MahasiswaApps
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
