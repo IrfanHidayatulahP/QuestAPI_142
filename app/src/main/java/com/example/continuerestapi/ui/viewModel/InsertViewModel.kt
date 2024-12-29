18462ee904714a15509ff9a743b57b0da1a3bfd4
IrfanHidayatulahP <irfanhputra400@gmail.com>
2024-12-29 18:20:23 +0700

    melengkapi class InsertViewModel membuat fungsi updateInsertMhsState dan insertMhs didalam class InsertViewModel

--- app/src/main/java/com/example/continuerestapi/ui/viewModel/InsertViewModel.kt
+++ app/src/main/java/com/example/continuerestapi/ui/viewModel/InsertViewModel.kt
@@ -4,11 +4,27 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.continuerestapi.model.Mahasiswa
 import com.example.continuerestapi.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
 
 class InsertViewModel(private val mhs : MahasiswaRepository) : ViewModel() {
     var uiState by mutableStateOf(InsertUiState())
+
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent) {
+        uiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertMhs() {
+        viewModelScope.launch {
+            try {
+                mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
 }
 
 data class InsertUiState(
