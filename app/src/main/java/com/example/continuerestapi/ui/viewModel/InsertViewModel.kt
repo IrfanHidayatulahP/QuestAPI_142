f34266c6fc34295a1d441208acf0b2c7de0f5f2c
IrfanHidayatulahP <irfanhputra400@gmail.com>
2024-12-29 18:17:17 +0700

    membuat fungsi InsertUiEvent.toMhs membuat data class InsertUiEvent dan InsertUiState

--- app/src/main/java/com/example/continuerestapi/ui/viewModel/InsertViewModel.kt
+++ app/src/main/java/com/example/continuerestapi/ui/viewModel/InsertViewModel.kt
@@ -11,7 +11,28 @@ class InsertViewModel(private val mhs : MahasiswaRepository) : ViewModel() {
     var uiState by mutableStateOf(InsertUiState())
 }
 
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent()
+)
 
+data class InsertUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = "",
+
+)
+
+fun InsertUiEvent.toMhs() : Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
 
 fun Mahasiswa.toUiStateMhs() : InsertUiState = InsertUiState(
     insertUiEvent = toInsertUiEvent()
